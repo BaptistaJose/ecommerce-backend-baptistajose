1fd336203cfe0917e8974f836b43064fdeaa245f
BaptistaJose <baptistaj133@gmail.com>
2025-07-27 19:14:19 -0300

    Remove .env.development del control de versiones

--- back/ecommerce-BaptistaJose/src/Users/users.service.ts
+++ back/ecommerce-BaptistaJose/src/Users/users.service.ts
@@ -6,27 +6,27 @@ import { User } from "./enitities/users.entity";
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  getUsers() {
-    return this.usersRepository.getUsers();
+  async getUsers() {
+    return await this.usersRepository.getUsers();
   }
 
-  getUserById(id: string) {
-    return this.usersRepository.getUserById(id);
+  async getUserById(id: string) {
+    return await this.usersRepository.getUserById(id);
   }
 
-  getUserByEmail(email: string) {
-    return this.usersRepository.getUserByEmail(email);
+  async getUserByEmail(email: string) {
+    return await this.usersRepository.getUserByEmail(email);
   }
 
   async createUser(user: Partial<User>) {
     return await this.usersRepository.createUser(user);
   }
 
-  updateUser(id: string, userUpdate: Partial<User>) {
-    return this.usersRepository.updateUser(id, userUpdate);
+  async updateUser(id: string, userUpdate: Partial<User>) {
+    return await this.usersRepository.updateUser(id, userUpdate);
   }
 
-  deleteUser(id: string) {
-    return this.usersRepository.deleteUser(id);
+  async deleteUser(id: string) {
+    return await this.usersRepository.deleteUser(id);
   }
 }
--- back/ecommerce-BaptistaJose/src/auth/auth.service.spec.ts
+++ back/ecommerce-BaptistaJose/src/auth/auth.service.spec.ts
@@ -1,12 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../Users/enitities/users.entity';
+import { UsersService } from '../Users/users.service';
+import { Role } from '../Users/enum/role.enum';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockUser: User = {
+    id: 'iu7676-isis08-ususu880',
+      name: 'pepito',
+      email: 'pepito@gmail.com',
+      password: '123456',
+      address: 'akjgbdhajks',
+      phone: '3263526',
+      country: 'nasj',
+      city: 'Ciudad falsa',
+      orders: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      role: Role.User 
+  }
+  const mockUsersService: Partial<UsersService> ={
+    getUserByEmail: () => Promise.resolve(mockUser),
+    createUser: (user: Partial<User>):  Promise<User> => Promise.resolve({
+      ...mockUser,
+      ...user 
+    } )
+  } 
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, JwtService,{
+        provide: UsersService,
+        useValue: mockUsersService
+      }],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
