bb04e962328944e7587eb24bce21ababa704e549
BaptistaJose <baptistaj133@gmail.com>
2026-02-10 00:09:31 -0300

    habilitación del cors para las peticiones del front

--- back/ecommerce-BaptistaJose/src/main.ts
+++ back/ecommerce-BaptistaJose/src/main.ts
@@ -5,18 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.FRONTEND_URL })
   app.useGlobalPipes(new ValidationPipe());
   app.use(LoggerMiddleware);
-    const swaggerConfig = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('My API demo nestjs')
     .setDescription('Esta es una API E-commerce creada con NestJS para ser empleada en la especialización de backend de Henry.')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api',app, document)
+  SwaggerModule.setup('api', app, document)
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
